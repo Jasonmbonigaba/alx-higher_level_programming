89798bc50fa695e63087a441fc75811cfebcdb45
Jason Mbonigaba <117821310+Jasonmbonigaba@users.noreply.github.com>
2023-06-14 17:00:10 +0200

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
-  elem => console.log(elem)
-);
+// It prints 3 lines
+
+console.log('C is fun\nPython is cool\nJavascript is amazing');
